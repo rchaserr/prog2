3142f7045534940d55eb462bafbbadce801ae0df
Ronaldo <alunoti@RJNVAESL02012.estacio.acad>
2018-04-17 21:06:55 -0300

    adicionando mapeamento das classes

--- src/br/com/java/academia/model/Funcionario.java
+++ src/br/com/java/academia/model/Funcionario.java
@@ -1,15 +1,29 @@
 
 package br.com.java.academia.model;
 
+import javax.persistence.*;
 
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
 public abstract class Funcionario extends Pessoa {
     
-    private long idFunc;
+    @Column
+    private float salario;
+        
     public Funcionario(){
         super();
     }
     
-    public Funcionario(String nome, String cpf) {
+    public Funcionario(String nome, String cpf, float salario) {
         super(nome, cpf);
+        this.salario = salario;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
     }
 }
